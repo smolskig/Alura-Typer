a164051dc8e0d686db4ad03f24332e262beb0da5
smolskig <1gabego@gmail.com>
2019-10-03 11:10:58 -0300

    progresso do modulo em 50%

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,58 @@
-var palavra = $(".frase").text();
-var num_palavras = palavra.split(" ").length
+var tempoPadrao = $("#tempo-digitacao").text();
+var campo = $(".campo-digitacao")
 
-var tamanhoFrase = $("#tamanho-frase")
-tamanhoFrase.text(num_palavras)
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContador();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciarJogo);
+})
+
+function atualizaTamanhoFrase(){
+    var palavra = $(".frase").text();
+    var num_palavras = palavra.split(" ").length
+
+    var tamanhoFrase = $("#tamanho-frase")
+    tamanhoFrase.text(num_palavras)
+}
+
+function inicializaContador(){
+    campo.on("input",function(){
+        var conteudo = campo.val()
+        var qtdPalavras = conteudo.split(/\S+/).length -1
+        $("#contador-palavras").text(qtdPalavras)
+        var qtdLetras = conteudo.length
+        $("#contador-caracteres").text(qtdLetras)  
+    })
+}
+
+
+
+function inicializaCronometro(){
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus",function(){
+        $("#botao-reiniciar").attr("disabled",true)
+        var cronometroID = setInterval(function(){
+            tempoRestante --;
+            $("#tempo-digitacao").text(tempoRestante)
+
+            if (tempoRestante < 1){
+                campo.attr("disabled",true)
+                clearInterval(cronometroID)
+                $("#botao-reiniciar").attr("disabled",false)
+            }
+        },1000)
+    })
+}
+
+
+
+function reiniciarJogo(){
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0")
+    $("#tempo-digitacao").text(tempoPadrao);
+    inicializaCronometro();
+}
+
