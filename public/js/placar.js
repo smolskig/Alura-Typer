d622c55226d13808abcd91c94a584e7b5757f940
smolskig <1gabego@gmail.com>
2019-10-15 10:43:33 -0300

    inicio do segundo modulo de Jquery

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,3 +1,5 @@
+$("#botao-placar").click(mostraPlacar)
+
 function inserePlacar(){
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Gabriel";
@@ -7,12 +9,26 @@ function inserePlacar(){
     linha.find(".botao-remover").click(removeLinha)
     corpoTabela.prepend(linha);
     
-   
+    $(".placar").slideDown(500);
+    scrollPlacar();
+}
+
+function scrollPlacar(){
+    var posicaoPlacar = $(".placar").offset().top;
+    $("html, body").animate(
+        {
+            scrollTop:posicaoPlacar+"px"
+        },1000);
 }
 
 function removeLinha(){
     event.preventDefault();
-    $(this).parent().parent().remove();
+
+    var linha = $(this).parent().parent()
+    linha.fadeOut(1000)
+    setTimeout(function(){
+        linha.remove();
+    },1000)
 }
 function novaLinha(usuario,palavras){
     var linha = $("<tr>");
@@ -33,3 +49,7 @@ function novaLinha(usuario,palavras){
     return linha;
     
 };
+
+function mostraPlacar(){
+    $(".placar").stop().slideToggle(600)
+}
